7ee1822f08a8bdb5ae9d2f4a3340a252fe386e6f
Ulises <ulises.aviles@cetys.edu.mx>
2022-02-15 12:43:40 -0800

    Modified some things

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,11 +10,11 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>STIMB's website</p>
+        <p>(This is a sample website.)</p>
+        <Link href="/posts/first-post">
+          <a>First post</a>
+        </Link>
       </section>
     </Layout>
   );
